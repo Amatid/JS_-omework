98b2b9391425b3293a571ee14dd0d7239907f9b9
Amatid <Dimagrodno@yandex.ru>
2020-11-11 20:54:52 +0300

    Задача 3

--- control1/control.js
+++ control1/control.js
@@ -7,16 +7,41 @@ function getMoreThanNull(arr) {
             continue;
         }
         newArr[j] = arr[i];
-        j++;  
+        j++;
     }
     return newArr;
 }
 
 //Задание 2
-function transformObject(obj){
-    for (var k in obj){
+function transformObject(obj) {
+    for (var k in obj) {
         var long = obj[k].length;
         obj[k + '_length'] = long;
         delete obj[k];
     }
+}
+
+//Задание 3
+function transformObject(obj) {
+    var dupObj = {};
+        
+    for (var k in obj) {
+        var long = obj[k].length;
+        if (dupObj[obj[k]] === undefined) {
+            dupObj[obj[k]] = 1;
+        } else {
+            dupObj[obj[k]]++;
+        }
+        obj[k + '_length'] = long;
+        delete obj[k];
+    }
+    for (var key in dupObj) {
+        if (dupObj[key] === 1){
+            delete dupObj[key];
+        } else {
+            dupObj[key] = dupObj[key] + ' times';
+        }
+    }
+    obj['Duplicated values'] = dupObj; 
+    return obj;
 }
